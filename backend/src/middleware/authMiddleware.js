5b283176ce86f5cb7715ce9ba82610099987ec41
ericpfranca <ericpfranca@gmail.com>
2025-03-30 17:15:25 -0300

    implementado middleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,1 +1,17 @@
-// arquivo de middleware
+import jwt from 'jsonwebtoken'
+
+export const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token
+
+  if (!token) {
+    return res.status(401).json({ message: 'Acesso negado. Não autenticado.' })
+  }
+
+  try {
+    const decoded = jwt.verify(token, 'secret')
+    req.user = decoded
+    next()
+  } catch (error) {
+    res.status(401).json({ message: 'Token inválido.' })
+  }
+}
