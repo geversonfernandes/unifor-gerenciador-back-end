a736cce4de5a2726ff3ca3328483a64b95166c81
bruno-brn <123398572+bruno-brn@users.noreply.github.com>
2025-03-30 17:33:41 -0300

    adicionado rota de eventos

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,6 +20,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
+app.use('/api/events', eventRoutes)
 app.use('/api/user', userRoutes)
 
 app.use((req, res, next) => {
