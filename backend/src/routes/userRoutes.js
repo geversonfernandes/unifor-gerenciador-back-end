9e2d102dc09589a6a6e4b4f64939b92af2823343
ericpfranca <ericpfranca@gmail.com>
2025-03-30 17:17:57 -0300

    expondo as rotas

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import {
   createUser,
+  updateUser,
+  deleteUser,
+  loginUser,
+  logoutUser,
 } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.post('/register', createUser)
+
+router.put('/users/:id', updateUser)
+
+router.delete('/users/:id', deleteUser)
+
+router.post('/login', loginUser)
+
+router.post('/logout', logoutUser)
 
 export default router
